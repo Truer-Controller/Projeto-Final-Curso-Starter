0bcc4ffe1cdc759885164d13fec4c1ffb94aa463
Rafael Augusto <rafajunior2907@gmail.com>
2022-01-26 15:29:40 -0300

    Add project files.

---
+++ projetofinalgft/Classes/EntidadeBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projetofinalgft.Classes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ projetofinalgft/Enum/Genero.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projetofinalgft.Enum
+{
+    public enum Genero
+    {
+        Acao = 1,
+
+        Aventura = 2,
+
+        Comedia = 3,
+
+        Documentario = 4,
+
+        Drama = 5,
+
+        Espionagem = 6,
+
+        Faroeste = 7,
+
+        Fantasia = 8,
+
+        Ficc_Cientifica = 9,
+
+        Musica = 10,
+
+        Romance = 11,
+
+        Suspense = 12,
+
+        Terror = 13
+
+    }
+}
